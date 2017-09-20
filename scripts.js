ef0ebe3f14bf054e9d8a8720b386db628be60a7b
derekpastor <derekjonp@gmail.com>
2017-09-19 23:57:32 -0400

    added photos

--- scripts.js
+++ scripts.js
@@ -4,9 +4,19 @@ function start(){
 $("#header").attr('style','min-height:+'+($(window).height() +25)+'px');
 
 $("#header").attr('style','min-width:+'+$(window).width()+'px');
+$('.grid').masonry({
+  // options...
+  itemSelector: '.grid-item',
+  columnWidth: 1000
+});
 }
-$( window ).resize(function() {
+$(window).resize(function() {
   $("#header").attr('style','min-height:+'+($(window).height() +100)+'px');
-
   $("#header").attr('style','min-width:+'+$(window).width()+'px');
-}
+});
+$(document).ready(function(){
+  $('ul.first').bsPhotoGallery({
+    "classes" : "col-lg-2 col-md-4 col-sm-3 col-xs-4 col-xxs-12",
+    "hasModal" : true
+  });
+});
