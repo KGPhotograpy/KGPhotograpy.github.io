7f3cf576724e523d3800f8724113b225f7000b61
Dr-D12345 <derekjonp@gmail.com>
2017-09-25 14:26:20 -0400

    new compresstion method for speed

--- scripts.js
+++ scripts.js
@@ -1,12 +1,28 @@
 
 console.log("hi");
+
 function start(){
   console.log("width:"+$(window).width()+ "height:"+$(window).height());
+var myDiv = $("#header");
+var src ="https://firebasestorage.googleapis.com/v0/b/brigade-929f9.appspot.com/o/headerimg.jpg?alt=media&token=880fe805-1914-40e2-9641-2223fbc92736";
 
+    var downloadingImage = new Image();
+    downloadingImage.onload = function(){
+        image.src = this.src;
 
-$("#header").attr('style','max-width:'+$(window).width()+'px;height:'+$(window).height() +'px');
+    };
+    downloadingImage.src = "https://firebasestorage.googleapis.com/v0/b/brigade-929f9.appspot.com/o/headerimg.jpg?alt=media&token=880fe805-1914-40e2-9641-2223fbc92736";
+
+$("#header").attr('style','max-width:'+$(window).width()+'px;height:'+$(window).height() +'px;  background-image: url("https://firebasestorage.googleapis.com/v0/b/brigade-929f9.appspot.com/o/headerimg.jpg?alt=media&token=880fe805-1914-40e2-9641-2223fbc92736")');
+
 
 }
+$(document).ready(function () {
+
+});
+
+
+
 $(window).resize(function() {
 start();
 });
